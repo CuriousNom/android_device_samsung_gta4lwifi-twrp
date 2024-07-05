d1e75b6b7bd6b8a99319864d42c2110822d685ed
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2024-07-05 13:17:15 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta4l-user
-add_lunch_combo omni_gta4l-userdebug
-add_lunch_combo omni_gta4l-eng
+add_lunch_combo omni_gta4lwifi-user
+add_lunch_combo omni_gta4lwifi-userdebug
+add_lunch_combo omni_gta4lwifi-eng
